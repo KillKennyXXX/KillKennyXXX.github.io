35d623c5711e6517dcc076b692bf82d18b26547f
Alexey Shalygin <84471084+KillKennyXXX@users.noreply.github.com>
2022-05-24 16:50:42 +0300

    main

--- js/myScript.js
+++ js/myScript.js
@@ -13,10 +13,10 @@ $(document).ready(function(){
     }
     }
 
-
-
-
-
-
     } 
+});
+
+$('a[href^="#"').click(function(){
+    let valHref = $(this).attr("href")
+    $('html, body').animate({scrollTop: $(valHref).offset.top - 50 + "px"});
 });
