cb416d36566a5fb6d77b2f5d5beecb11d75eb0dc
Alexey Shalygin <84471084+KillKennyXXX@users.noreply.github.com>
2022-05-25 12:10:05 +0300

    main

--- js/myScript.js
+++ js/myScript.js
@@ -1,22 +1,12 @@
 
-$(document).ready(function(){
 
-    $(window).scroll() =>  {
-    let scrollDistance = $(window).scrollTop();
-    $(".section").each(i, el) => {
-        if($(el).offset().top() - $("nav").outerHeight() <= scrollDistance) {
-            $("nav a").each(i, el) => {
-                if($(el).hasClass("active")){
-                    $(el).removeClass("active");
-                }
-            }
-    }
-    }
-
-    } 
-});
 
 $('a[href^="#"').click(function(){
-    let valHref = $(this).attr("href")
-    $('html, body').animate({scrollTop: $(valHref).offset.top - 50 + "px"});
+    let valHref = $(this).attr("href");
+    $('html, body').animate({scrollTop: $(valHref).offset().top - 50 + "px"});
 });
+
+
+
+
+
